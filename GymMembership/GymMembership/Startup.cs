430d9afc1a6133cea11203536b5d7d262eeb4da1
Rahul <mr.rahulsingh@hotmail.com>
2017-12-28 01:05:41 +0100

    Created Repository

---
+++ GymMembership/GymMembership/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymMembership.Startup))]
+namespace GymMembership
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
