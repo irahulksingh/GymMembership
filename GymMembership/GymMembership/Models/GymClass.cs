b42aea7c35a6edb6029f7a8886257e21e20df515
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-12-28 11:13:24 +0100

    Added GymClass

---
+++ GymMembership/GymMembership/Models/GymClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Web;
+
+namespace GymMembership.Models
+{
+    public class GymClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Column (TypeName ="Datetime2")]
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        [Column(TypeName = "Datetime2")]
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public string Description { get; set; }
+
+
+    }
+}
